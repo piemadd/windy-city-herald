18904dbda1e09da9c97b029e26dc0bcbd8ddd0a4
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-06-28 22:09:51 -0500

    Fixed hotbox title and titlecase function

--- build.js
+++ build.js
@@ -9,7 +9,7 @@ const ejs = require('ejs');
 marked.use(mangle());
 
 const titleCase = (str) => {
-  return str.toLowerCase().split(' ').map(function (word) {
+  return str.split(' ').map(function (word) {
     return word.replace(word[0], word[0].toUpperCase());
   }).join(' ');
 };
